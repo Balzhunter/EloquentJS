69bcf20597f239c24d809216ae9ea6760d4d0d80
Balzhunter <56179042+Balzhunter@users.noreply.github.com>
2021-04-18 00:09:26 -0500

    Cap 3 Added exponential
    Exponential function added

--- assets/cap3.js
+++ assets/cap3.js
@@ -2,3 +2,11 @@
 const menos = (a,b) => b===undefined?-a:a-b; 
 console.log(menos(4));
 console.log(menos(4,2));
+//FUNCTION EXPONENTIAL
+const pot = (base, exp = 2) => {
+    let out = 1;
+    for(i=0;i<exp;i++) out*=base;
+    return out;
+}
+console.log(pot(4))
+console.log(pot(2,6))
